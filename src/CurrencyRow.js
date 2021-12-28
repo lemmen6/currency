4e5f21872455208877e763ae52fd911b00fee171
Lodewijk Emmen <lodewijk.emmen@nafite.com>
2021-12-28 12:38:11 +0100

    first commit

---
+++ src/CurrencyRow.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function CurrencyRow(props) {
+    const { currencyOptions, selectedCurrency, onChangeCurrency, amount, onChangeAmount } = props;
+    return (
+        <div>
+            <input className="input" type="number" value={amount} onChange={onChangeAmount}/>
+            <select value={selectedCurrency} onChange={onChangeCurrency}>
+                {currencyOptions.map(option => (
+                <option key={option} value={option}>{option}</option>
+                ))}
+            </select>
+        </div>
+    )
+}
